975d14355d0d918934256a464cf0b3d1be0c5c1a
Bo Nicolajsen <>
2024-02-26 17:24:27 +0100

    solution

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,22 @@
 // write cool JS hwere!!
+const menuData = {
+
+}
+
+let myMenuItems = document.getElementById("menuItems");
+
+
+function menuButton() {
+    myMenuItems.classList.toggle('hidden')
+}
+
+function menuCallBack(menuItem) {
+    console.log(menuItem);
+    myMenuItems.classList.toggle('hidden')
+}
+
+
+
+
+
+
